9e10236f2ace2bb8dfa58d07b4dfbeebc40ea026
BSick7 <brad.sickles@gmail.com>
2015-01-08 10:46:00 -0500

    Now using logger instead of passing in renderer.  Now asking for inclusion of typings.

--- bin/unify.js
+++ bin/unify.js
@@ -7,8 +7,5 @@ var optimist = require('optimist'),
     faydeunify = require('../lib');
 
 var argv = optimist.boolean(['v', 'verbose']).argv;
-var config = {
-    verbose: argv.v || argv.verbose
-};
 
-faydeunify.interactive(argv._[0], config, new faydeunify.JsonFile("unify.json"));
+faydeunify.interactive(argv._[0], new faydeunify.JsonFile("unify.json"));
